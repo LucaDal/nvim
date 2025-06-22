c4a28910b1f4f75b898cdf51442fc74a390d2020
LucaDal <dalessandroluca1@gmail.com>
2025-06-22 18:34:20 +0200

    Init configuration

---
+++ lua/config/remap.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = 'back to [P]roject [V]iew'})
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+--permette di sostituire la stringa selezionata
+vim.keymap.set("n", "<leader>ss", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+
